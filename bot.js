7835f9253b65e8cca8d67c5d7022deb5022930ae
Finx1 <49606811+Finx1@users.noreply.github.com>
2019-05-21 14:48:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,51 +3,50 @@ var fs = require('fs');
 var client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
+  console.log(Welcome Bro ${client.user.tag}!);
 });
 var prefix = 'F!'
-
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id !== "285760610010923008") return;
 
-  
-  if (message.content.startsWith(prefix + 'setwatch')) {
+
+  if (message.content.startsWith(prefix + 'wt')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`Watch Now: **${argresult}`)
+    message.channel.sendMessage(Watch Now: تم تغيير الحالة الي${argresult})
 } 
 
  
-  if (message.content.startsWith(prefix + 'setlis')) {
+  if (message.content.startsWith(prefix + 'ls')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
+    message.channel.sendMessage(LISTENING Now: تم تغيير الحالة الي${argresult})
 } 
 
 
 if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+      message.channel.sendMessage(Username Changed To تم تغيير الاسم الي${argresult}**)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+   message.channel.sendMessage(Avatar Changed Successfully To تم تغييرالصورة الي${argresult}**);
 }
 
-if (message.content.startsWith(prefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
+if (message.content.startsWith(prefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/peery13%22);
      console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
+    message.channel.sendMessage(Streaming: تم تغيير الحالة الي${argresult})
 } 
-if (message.content.startsWith(prefix + 'setgame')) {
+if (message.content.startsWith(prefix + 'ply')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
-    message.channel.sendMessage(`Playing: **${argresult}`)
+    message.channel.sendMessage(Playing: تم تغير الحالة الي${argresult})
 } 
 
 
